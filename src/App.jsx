6635ef2bd6bad0972d86b6bc2ba6be7f4dda758f
Steve Kimoi <moenisteve@gmail.com>
2022-10-13 08:40:20 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,63 @@
 import { useState, useEffect } from 'react'; 
 import './App.css'
 import name from '@rstacruz/startup-name-generator'; 
+import Heading from './components/Heading';
 
 function App() {
   const [inputName, setInputName] = useState(""); 
   const [suggestedNames, setSuggestedNames] = useState([]); 
   const [displayNames, setDisplayNames] = useState([]); 
+  const [inputActive, setInputActive] = useState(false); 
+  // const [displayNamesAvailable, setDisplayNamesAvailable] = useState(false); 
 
   const handleChange = (e) => {
     setInputName(e.target.value); 
     setSuggestedNames(name(inputName));  
   }
 
+  const handleButtonClean = (e) => {
+    e.preventDefault(); 
+    setInputName("");
+    setDisplayNames([]);
+    setInputActive(false); 
+  }
+
   useEffect( () => {
     setDisplayNames(suggestedNames.slice(0,25)); 
-  }, [inputName])
-  
+  }, [inputName]); 
+
 
   return (
     <div className="app">
 
-      <div className='heading'>
-          <h1>NAME YOUR WEBSITE</h1> 
-          <p>Input your preferred website name and get over 20+ suggestions<br/>Click on your preferred name and get to see available domain names</p>
-      </div>
+      <Heading inputActive={inputActive} />
 
       <form className='app-form'>
-        <label>Proposed name</label> 
-        <input type="text" onChange={handleChange} /> 
+        <input type="text" placeholder="Proposed name" onChange={handleChange} onFocus={ () => setInputActive(true)}/> 
+        <button className='button-clean' onClick={handleButtonClean}>Clean</button>
       </form>
 
-      <div className='displayed-names'>
-        {
-          displayNames.map( name => {
-            return (
-              <div key={name} className="single-name"> 
-                <a
-                  href={`https://www.namecheap.com/domains/registration/results/?domain=${name}`}
-                  target="_blank"
-                >
-                  { name }
-                </a>
-              </div>
-            )
-          })
-        }
-      </div>
+      {
+        displayNames.length !== 0 && (
+            <div className='displayed-names'>
+                {
+                  displayNames.map( name => {
+                    return (
+                      <div key={name} className="single-name"> 
+                        <a
+                          href={`https://www.namecheap.com/domains/registration/results/?domain=${name}`}
+                          target="_blank"
+                        >
+                          { name }
+                        </a>
+                      </div>
+                    )
+                  })
+                }
+            </div>
+        )
+      }
+
     </div>
   )
 }
